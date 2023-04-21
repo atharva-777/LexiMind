94426696b90bfe7d4b32800bda07174c533824ac
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-04-21 23:47:46 +0530

    Done initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>Welcome to LexiMind !</h2>
+        <p>Your destination for doing anything with text 🔜</p>
+      </div>
+    </>
+  );
+}
+
+export default App
