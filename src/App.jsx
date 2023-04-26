057d9a9c30b8e6a1c6ecd48d75a5cbd0344f3630
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-04-26 23:57:05 +0530

    Done with component structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ function App() {
 
   return (
     <>
-      <div>
+      <div style={{ textAlign: "center" }}>
         <h2>Welcome to LexiMind !</h2>
         <p>Your destination for doing anything with text 🔜</p>
+        <p>
+          Unleash the Power of Words with LexiMind: Your Ultimate Text Companion
+        </p>
       </div>
     </>
   );
