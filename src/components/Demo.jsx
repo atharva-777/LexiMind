b30d2713c32c758a80aa3d7993ed1c590ed9b4b4
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-05-01 17:40:21 +0530

    api is working properly

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,49 @@
 // import React from 'react'
+import { useState } from "react";
+import { useLazyGetSummaryQuery } from "../store/article";
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-export default Demo
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit  = async (e) => {
+    e.preventDefault();
+     const {data} = await getSummary({articleUrl:article.url});
+
+    if(data?.summary){
+      const newArticle = {...article,summary:data.summary};
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
+
+
+
+    //  setArticle({...article,summary:data.summary})
+  };
+
+  return (
+    <section>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="url"
+            placeholder="paste the article link"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            required
+          />
+          <button type="submit">✅</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
--- src/store/article.js
+++ src/store/article.js
@@ -1,13 +1,18 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const API_KEY = import.meta.env.API_KEY
+const rapidapikey = import.meta.env.API_KEY; 
+// api key is not working in this way
+// it requires to add directly to this file 
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", API_KEY);
+      headers.set(
+        "X-RapidAPI-Key",
+        rapidapikey
+      );
       headers.set(
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
