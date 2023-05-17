57a167f988461c2744d36a8fc4f8092d7f8dad15
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-05-17 23:47:57 +0530

    Hidden api key

--- src/store/article.js
+++ src/store/article.js
@@ -1,8 +1,6 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const rapidapikey = import.meta.env.API_KEY; 
-// api key is not working in this way
-// it requires to add directly to this file 
+const rapidapikey = import.meta.env.VITE_API_KEY; 
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
